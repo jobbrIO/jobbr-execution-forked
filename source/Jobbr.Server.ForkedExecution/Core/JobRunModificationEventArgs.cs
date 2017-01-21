3b9fcfb0e81bb6a0b1f6e425bfd729e0510be8f0
Michael Schnyder <michael.schnyder@outlook.com>
2017-01-21 12:19:39 +0100

    moved to this repo

---
+++ source/Jobbr.Server.ForkedExecution/Core/JobRunModificationEventArgs.cs
@@ -0,0 +1,15 @@
+﻿//using Jobbr.Common.Model;
+
+//namespace Jobbr.Server.Core
+//{
+//    using System;
+
+//    public class JobRunModificationEventArgs : EventArgs
+//    {
+//        public Job Job { get; set; }
+
+//        public JobTriggerBase Trigger { get; set; }
+
+//        public JobRun JobRun { get; set; }
+//    }
+//}
---
+++ source/Jobbr.Server.ForkedExecution/Core/JobTriggerEventArgs.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+
+//using Jobbr.Common.Model;
+
+//namespace Jobbr.Server.Core
+//{
+//    /// <summary>
+//    /// The job trigger event args.
+//    /// </summary>
+//    public class JobTriggerEventArgs : EventArgs
+//    {
+//        public JobTriggerBase Trigger { get; set; }
+//    }
+//}
