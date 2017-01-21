9d6e1564ab2164ffbfe91d8130ffc6dee7dff2fa
Michael Schnyder <michael.schnyder@outlook.com>
2017-01-21 12:47:00 +0100

    improved logging

--- source/Jobbr.Server.ForkedExecution/Core/ForkedJobExecutor.cs
+++ source/Jobbr.Server.ForkedExecution/Core/ForkedJobExecutor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Jobbr.ComponentModel.Execution;
 using Jobbr.ComponentModel.Execution.Model;
@@ -14,6 +15,8 @@ namespace Jobbr.Server.ForkedExecution.Core
 
         private static readonly ILog Logger = LogProvider.For<ForkedJobExecutor>();
 
+        private static long MaxWebHostStartWait = 2000;
+
         public ForkedJobExecutor(IJobRunInformationService jobRunInformationService, ForkedExecutionConfiguration configuration)
         {
             this.jobRunInformationService = jobRunInformationService;
@@ -27,10 +30,24 @@ namespace Jobbr.Server.ForkedExecution.Core
 
         public void Start()
         {
+            Logger.Debug($"Starting ForkedJobExecutor asynchonously...");
+            var duration = new Stopwatch();
+            duration.Start();
+
             Task.Run(async () =>
             {
-                await Task.Delay(900);
-                Logger.Debug($"Starting ForkedJobExecutor. Backchannel will be available @ '{this.configuration.BackendAddress}'");
+                while (string.IsNullOrWhiteSpace(this.configuration.BackendAddress) && duration.ElapsedMilliseconds < MaxWebHostStartWait)
+                {
+                    await Task.Delay(200);
+                }
+                duration.Stop();
+
+                if (!string.IsNullOrWhiteSpace(this.configuration.BackendAddress) || duration.ElapsedMilliseconds > MaxWebHostStartWait)
+                {
+                    throw new Exception("Unable to information about the BackChannel Connection readyness. Executor will be in unhealth state");
+                }
+
+                Logger.Debug($"ForkedJobExecutor started. Backchannel will be available @ '{this.configuration.BackendAddress}'");
             });
         }
 
@@ -46,7 +63,7 @@ namespace Jobbr.Server.ForkedExecution.Core
 
         public bool OnJobRunCanceled(Guid uniqueId)
         {
-            return true;
+            return false;
         }
     }
 }
