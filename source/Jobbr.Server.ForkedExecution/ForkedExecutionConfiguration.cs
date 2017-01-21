dd47c8a46f5d1f6fe27ca4f325a1079312e8836b
Michael Schnyder <michael.schnyder@outlook.com>
2017-01-21 17:52:33 +0100

    added related configuration possibilities

--- source/Jobbr.Server.ForkedExecution/ForkedExecutionConfiguration.cs
+++ source/Jobbr.Server.ForkedExecution/ForkedExecutionConfiguration.cs
@@ -1,7 +1,21 @@
-﻿namespace Jobbr.Server.ForkedExecution
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jobbr.Server.ForkedExecution
 {
     public class ForkedExecutionConfiguration
     {
         public string BackendAddress { get; set; }
+
+        public int MaxConcurrentJobs { get; set; } = 4;
+
+        public Func<string> JobRunnerExeResolver { get; set; }
+
+        public string JobRunDirectory { get; set; }
+
+        public bool IsRuntimeWaitingForDebugger { get; set; }
+
+        public Func<string, string, IEnumerable<KeyValuePair<string, string>>> CustomJobRunnerParameters { get; set; }
+
     }
 }
