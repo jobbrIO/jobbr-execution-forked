9ddec42f1af660e289e88298bbe626773a8e0970
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-01-23 20:12:00 +0100

    additional simple test that fail

--- source/Jobbr.Server.ForkedExecution.Tests/ExecutionTests.cs
+++ source/Jobbr.Server.ForkedExecution.Tests/ExecutionTests.cs
@@ -1,5 +1,9 @@
 ﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using Jobbr.ComponentModel.Execution.Model;
 using Jobbr.Server.ForkedExecution.BackChannel;
 using Jobbr.Server.ForkedExecution.Tests.Infrastructure;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -53,6 +57,51 @@ namespace Jobbr.Server.ForkedExecution.Tests
             this.storedProgressUpdates.WaitForStatusUpdate(allUpdates => allUpdates[fakeRun.UniqueId].Contains(JobRunStates.Failed) || allUpdates[fakeRun.UniqueId].Contains(JobRunStates.Completed), 10000);
         }
 
+        [TestMethod]
+        public void RunnerExecutable_WithInvalidJob_ReturnsFailed()
+        {
+            // Setup
+            var config = GivenAMinimalConfiguration();
+            config.JobRunnerExeResolver = () => "Jobbr.Server.ForkedExecution.TestRunner.exe";
+            
+            this.GivenAStartedBackChannelHost(config);
+            var executor = this.GivenAStartedExecutor(config);
+
+            var fakeRun = this.jobRunFakeTuples.CreateFakeJobRun();
+            fakeRun.JobRunInfo.Type = "NotExistentClrType";
+
+            // Act
+            executor.OnPlanChanged(new List<PlannedJobRun>(new[] { fakeRun.PlannedJobRun }));
+
+            // Test
+            var hasCompleted = this.storedProgressUpdates.WaitForStatusUpdate(allUpdates => allUpdates[fakeRun.UniqueId].Contains(JobRunStates.Failed), 3000);
+
+            Assert.IsTrue(hasCompleted, "The runner executable should fail, but did not within 3s");
+            Assert.IsTrue(this.storedProgressUpdates.AllStatusUpdates[fakeRun.UniqueId].Contains(JobRunStates.Failed), "There should be a failed state for this jobRun");
+        }
+
+        [TestMethod]
+        public void RunnerExecutable_GetsValidJob_ReturnsCompleted()
+        {
+            // Setup
+            var config = GivenAMinimalConfiguration();
+            config.JobRunnerExeResolver = () => "Jobbr.Server.ForkedExecution.TestRunner.exe";
+
+            this.GivenAStartedBackChannelHost(config);
+            var executor = this.GivenAStartedExecutor(config);
+
+            var fakeRun = this.jobRunFakeTuples.CreateFakeJobRun();
+            fakeRun.JobRunInfo.Type = "SimpleJob";
+
+            // Act
+            executor.OnPlanChanged(new List<PlannedJobRun>(new[] { fakeRun.PlannedJobRun }));
+
+            // Test
+            var hasCompleted = this.storedProgressUpdates.WaitForStatusUpdate(allUpdates => allUpdates[fakeRun.UniqueId].Contains(JobRunStates.Completed), 3000);
+
+            Assert.IsTrue(hasCompleted, "The runner executable should completed, but did not within 3s");
+            Assert.IsTrue(this.storedProgressUpdates.AllStatusUpdates[fakeRun.UniqueId].Contains(JobRunStates.Completed), "There should be a completed state for this jobRun");
+        }
         }
     }
 }
