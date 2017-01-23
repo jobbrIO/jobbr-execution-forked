9a016e9e74de7053f0a425188634d0878c029cea
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-01-23 18:05:19 +0100

    empty tests for fulltest

---
+++ source/Jobbr.Server.ForkedExecution.Tests/ExecutionTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jobbr.Server.ForkedExecution.Tests
+{
+    [TestClass]
+    public class ExecutionTests
+    {
+        [TestMethod]
+        public void PlannedJob_AfterStart_WillCallback()
+        {
+            
+        }
+    }
+}
