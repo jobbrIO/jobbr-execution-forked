1cfb9f36189b1cec0223f787f2aa84e7c7edb49f
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-04-04 11:58:45 +0200

    added additional testcase which is inclonclusive for now

--- source/Jobbr.Server.ForkedExecution.Tests/JobExecutorOnPlanChangedTests.cs
+++ source/Jobbr.Server.ForkedExecution.Tests/JobExecutorOnPlanChangedTests.cs
@@ -147,27 +147,32 @@ namespace Jobbr.Server.ForkedExecution.Tests
             Assert.IsFalse(this.storedProgressUpdates.AllStatusUpdates.ContainsKey(fakeJobRun3.Id), "There should be no updates for the third job");
         }
 
+        [TestMethod]
         public void StartOverbooked_RemoveQueued_DoesNotStartRemoved()
         {
-            //// Setup
-            //var forkedExecutionConfiguration = GivenAMinimalConfiguration();
+            Assert.Inconclusive("Test is unreliable because it dependes on a race-condition and bug to not fail.");
+            // Setup
+            var forkedExecutionConfiguration = GivenAMinimalConfiguration();
 
-            //// Only run 2 jobs at a time
-            //forkedExecutionConfiguration.MaxConcurrentProcesses = 2;
-            //var executor = this.GivenAStartedExecutor(forkedExecutionConfiguration);
+            // Only run 2 jobs at a time
+            forkedExecutionConfiguration.MaxConcurrentProcesses = 2;
+            var executor = this.GivenAStartedExecutor(forkedExecutionConfiguration);
 
-            //// Act 1: Create & Send only first plan
-            //var fakeJobRun1 = this.jobRunFakeTuples.CreateFakeJobRun(DateTime.UtcNow);
-            //var fakeJobRun2 = this.jobRunFakeTuples.CreateFakeJobRun(DateTime.UtcNow);
-            //var fakeJobRun3 = this.jobRunFakeTuples.CreateFakeJobRun(DateTime.UtcNow);
+            // Act 1: Create & Send only first plan
+            var fakeJobRun1 = this.jobRunFakeTuples.CreateFakeJobRun(DateTime.UtcNow);
+            var fakeJobRun2 = this.jobRunFakeTuples.CreateFakeJobRun(DateTime.UtcNow);
+            var fakeJobRun3 = this.jobRunFakeTuples.CreateFakeJobRun(DateTime.UtcNow);
 
-            //executor.OnPlanChanged(new List<PlannedJobRun>(new[] { fakeJobRun1.PlannedJobRun, fakeJobRun2.PlannedJobRun, fakeJobRun3.PlannedJobRun }));
+            this.manualTimeProvider.AddSecond();
+            executor.OnPlanChanged(new List<PlannedJobRun>(new[] { fakeJobRun1.PlannedJobRun, fakeJobRun2.PlannedJobRun, fakeJobRun3.PlannedJobRun }));
 
-            //// Act 2: Send an empty plan
-            //executor.OnPlanChanged(new List<PlannedJobRun>());
+            this.storedProgressUpdates.WaitForStatusUpdate(allUpdates => allUpdates.SelectMany(j => j.Value).Count(s => s == JobRunStates.Completed) == 2, 6000);
 
-            //// Wait
-            //this.storedProgressUpdates.WaitForStatusUpdate(allUpdates => allUpdates.SelectMany(j => j.Value).Count(s => s == JobRunStates.Completed) == 2, 6000);
+            // Act 2: Send an empty plan
+            this.manualTimeProvider.AddSecond();
+            executor.OnPlanChanged(new List<PlannedJobRun>());
+
+            // Wait
         }
     }
 }
