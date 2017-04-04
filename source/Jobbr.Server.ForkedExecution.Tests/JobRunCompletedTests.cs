e7fdb4c3f5fc74a54259ed1b1fc45530d50dffd3
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-04-04 18:27:50 +0200

    added test retaled to #17

---
+++ source/Jobbr.Server.ForkedExecution.Tests/JobRunCompletedTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Jobbr.ComponentModel.Execution.Model;
+using Jobbr.Server.ForkedExecution.Tests.Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jobbr.Server.ForkedExecution.Tests
+{
+    [TestClass]
+    public class JobRunCompletedTests : TestBase
+    {
+        [TestMethod]
+        public void ProcessExits_NonZeroExitCode_StateIsFailed()
+        {
+            // Setup
+            var forkedExecutionConfiguration = GivenAMinimalConfiguration();
+            var executor = this.GivenAMockedExecutor(forkedExecutionConfiguration);
+
+            // Act
+            var fakeJobRun = this.jobRunFakeTuples.CreateFakeJobRun(DateTime.UtcNow);
+
+            this.manualTimeProvider.AddSecond();
+            executor.OnPlanChanged(new List<PlannedJobRun>(new[] { fakeJobRun.PlannedJobRun, }));
+            
+            this.jobRunContextMockFactory[fakeJobRun.Id].RaiseEnded(-3683737);
+
+            // Test
+            var allStatesForJob = this.storedProgressUpdates.AllStatusUpdates[fakeJobRun.Id];
+
+            Assert.AreEqual(2, allStatesForJob.Count, "There should be two transitions instead of a timeout");
+            Assert.AreEqual(JobRunStates.Failed, allStatesForJob[1], "The Mock should have issued the state 'Starting'");
+        }
+
+        [TestMethod]
+        public void ProcessExists_WithZeroExitCode_StateIsCompleted()
+        {
+            // Setup
+            var forkedExecutionConfiguration = GivenAMinimalConfiguration();
+            var executor = this.GivenAMockedExecutor(forkedExecutionConfiguration);
+
+            // Act
+            var fakeJobRun = this.jobRunFakeTuples.CreateFakeJobRun(DateTime.UtcNow);
+
+            this.manualTimeProvider.AddSecond();
+            executor.OnPlanChanged(new List<PlannedJobRun>(new[] { fakeJobRun.PlannedJobRun, }));
+
+            this.jobRunContextMockFactory[fakeJobRun.Id].RaiseEnded(0);
+
+            // Test
+            var allStatesForJob = this.storedProgressUpdates.AllStatusUpdates[fakeJobRun.Id];
+
+            Assert.AreEqual(2, allStatesForJob.Count, "There should be two transitions instead of a timeout");
+            Assert.AreEqual(JobRunStates.Completed, allStatesForJob[1], "The Mock should have issued the state 'Starting'");
+
+        }
+
+        [TestMethod]
+        public void ProcessExistsHadPercent_OnZeroExitCode_PercentIs100()
+        {
+            // Setup
+            var forkedExecutionConfiguration = GivenAMinimalConfiguration();
+            var executor = this.GivenAMockedExecutor(forkedExecutionConfiguration);
+
+            // Act
+            var fakeJobRun = this.jobRunFakeTuples.CreateFakeJobRun(DateTime.UtcNow);
+
+            this.manualTimeProvider.AddSecond();
+            executor.OnPlanChanged(new List<PlannedJobRun>(new[] { fakeJobRun.PlannedJobRun, }));
+
+            this.jobRunContextMockFactory[fakeJobRun.Id].RaiseProgressUpdate(54);
+            this.jobRunContextMockFactory[fakeJobRun.Id].RaiseEnded(0);
+
+            // Test
+            Assert.AreEqual(100, this.storedProgressUpdates.AllProgressUpdates[fakeJobRun.Id][1], "There should be a 100% message if the job has ended without error code");
+        }
+    }
+}
