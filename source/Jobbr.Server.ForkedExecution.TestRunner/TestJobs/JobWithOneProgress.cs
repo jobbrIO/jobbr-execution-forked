e9d2ce43aa94763316d804e0d4320cfea9ad75ff
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-04-05 13:19:49 +0200

    extended test to support failing ;)

--- source/Jobbr.Server.ForkedExecution.TestRunner/TestJobs/JobWithOneProgress.cs
+++ source/Jobbr.Server.ForkedExecution.TestRunner/TestJobs/JobWithOneProgress.cs
@@ -2,11 +2,21 @@
 
 namespace Jobbr.Server.ForkedExecution.TestRunner.TestJobs
 {
+    public class TestArguments
+    {
+        public bool ShouldFail { get; set; }
+    }
+
     public class JobWithOneProgress
     {
-        public void Run()
+        public void Run(object jobParams, TestArguments runParams)
         {
             Console.WriteLine("##jobbr[progress percent='42.135']");
+
+            if (runParams.ShouldFail)
+            {
+                Environment.Exit(-38464);
+            }
         }
     }
 }
