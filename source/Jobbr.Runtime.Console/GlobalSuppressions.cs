457f665898f138d68f7194be0d7a68d94ebd03d1
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-04-18 19:24:22 +0200

    fixed namespace

--- source/Jobbr.Runtime.Console/GlobalSuppressions.cs
+++ source/Jobbr.Runtime.Console/GlobalSuppressions.cs
@@ -3,4 +3,4 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2213:Disposable fields should be disposed", Justification = "LibLog is imported code and managed from external developers", Scope = "Type", Target = "Jobbr.Runtime.Console.Logging.LogProviders.LogProviderBase")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2213:Disposable fields should be disposed", Justification = "LibLog is imported code and managed from external developers", Scope = "Type", Target = "Jobbr.ConsoleApp.Runtime.Logging.LogProviders.LogProviderBase")]
