4a857f6810588d4fa03ef0758bcc28ce2a5a8af4
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-04-22 00:19:06 +0200

    early exit and added some more comments and logging info

--- source/Jobbr.Runtime.Core/CoreRuntime.cs
+++ source/Jobbr.Runtime.Core/CoreRuntime.cs
@@ -34,7 +34,7 @@ namespace Jobbr.Runtime.Core
         {
             this.jobInfo = jobRunInfo;
 
-            bool executionResult = false;
+            var executionResult = false;
 
             try
             {
@@ -42,38 +42,49 @@ namespace Jobbr.Runtime.Core
 
                 var jobTypeName = this.jobInfo.JobType;
 
+
+                // Resolve Type
                 Logger.Debug($"Trying to resolve the specified type '{jobTypeName}'...");
 
                 var type = this.jobTypeResolver.ResolveType(jobTypeName);
-
                 if (type == null)
                 {
                     Logger.Error($"Unable to resolve the type '{jobTypeName}'!");
+                    return;
                 }
-                else
+
+                // Activate Type
+                Logger.Debug($"Trying to activate the specified type '{type}'...");
+
+                var jobClassInstance = this.CreateJobClassInstance(jobTypeName, type);
+                if (jobClassInstance == null)
                 {
-                    var jobClassInstance = this.CreateJobClassInstance(jobTypeName, type);
-
-                    if (jobClassInstance != null)
-                    {
-                        var task = this.CreateRunTask(jobClassInstance);
-
-                        if (task != null)
-                        {
-                            Logger.Debug("Starting Task to execute the Run()-Method.");
-
-                            task.Start();
-                            this.PublishState(JobRunState.Processing);
-
-                            executionResult = this.WaitForCompletion(task);
-                        }
-                    }
+                    Logger.Error($"Unable to create an instance ot the type '{type}'!");
+                    return;
                 }
+
+                // Create task as wrapper for calling the Run() Method
+                Logger.Debug($"Create task as wrapper for calling the Run() Method");
+
+                var task = this.CreateRunTask(jobClassInstance);
+                if (task == null)
+                {
+                    Logger.Error("Unable to create task as a wrapper for the job");
+                    return;
+                }
+
+                // Start 
+                Logger.Debug("Starting Task to execute the Run()-Method.");
+
+                task.Start();
+                this.PublishState(JobRunState.Processing);
+
+                // Wait for completion
+                executionResult = this.WaitForCompletion(task);
             }
             catch (Exception e)
             {
                 Logger.FatalException("Exception in the Jobbr-Runtime. Please see details: ", e);
-
             }
             finally
             {
@@ -108,6 +119,7 @@ namespace Jobbr.Runtime.Core
             catch (Exception e)
             {
                 Logger.ErrorException("Exception while waiting for completion of job", e);
+                return false;
             }
 
             if (runTask.IsFaulted)
