ef4b836555156a1f893fb1b1f780296df3f5bb0e
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-04-22 10:27:00 +0200

    moved according to usage

--- source/Jobbr.Runtime.Core/JobRunInfo.cs
+++ source/Jobbr.Runtime.Core/JobRunInfo.cs
@@ -2,11 +2,11 @@ namespace Jobbr.Runtime.Core
 {
     public class JobRunInfo
     {
+        public string JobType { get; set; }
+
         public object JobParameter { get; set; }
 
         public object InstanceParameter { get; set; }
-
-        public string JobType { get; set; }
 
         public string UserId { get; set; }
 
