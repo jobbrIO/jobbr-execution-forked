8aafa5eb95b7936ddb2ed47d8a8c5fe21a048d1c
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-06-02 17:25:24 +0200

    added first tests against resolver

---
+++ source/Jobbr.Runtime.Core.Tests/JobTypeResolverTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using Jobbr.Runtime.Core.Activation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jobbr.Runtime.Core.Tests
+{
+    [TestClass]
+    public class JobTypeResolverTests
+    {
+        public class JobInExecutingAssembly
+        {
+            public void Run() { }
+        }
+
+        [TestMethod]
+        public void TypeFullName_SearchAssemblySet_TypeFound()
+        {
+            var jobTypeSearchAssembly = Assembly.GetExecutingAssembly();
+
+            var jobType = typeof(JobInExecutingAssembly).FullName;
+
+            var jobTypeResolver = new JobTypeResolver(jobTypeSearchAssembly);
+
+            var result = jobTypeResolver.ResolveType(jobType);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(typeof(JobInExecutingAssembly), result);
+        }
+
+        [TestMethod]
+        public void AssemblyFullqualifiedName_NoSearchAssemblySet_TypeFound()
+        {
+            var jobType = typeof(JobInExecutingAssembly).AssemblyQualifiedName;
+
+            var jobTypeResolver = new JobTypeResolver(null);
+
+            var result = jobTypeResolver.ResolveType(jobType);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(typeof(JobInExecutingAssembly), result);
+        }
+    }
+}
