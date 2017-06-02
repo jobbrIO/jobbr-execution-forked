dad049e0d84dd87a7e1a3781d6e5c025884b4062
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-06-02 17:25:07 +0200

    added test project as friend assembly

--- source/Jobbr.Runtime.Core/Properties/AssemblyInfo.cs
+++ source/Jobbr.Runtime.Core/Properties/AssemblyInfo.cs
@@ -34,3 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Make internals visible for the testproject
+[assembly: InternalsVisibleTo("Jobbr.Runtime.Core.Tests")]
